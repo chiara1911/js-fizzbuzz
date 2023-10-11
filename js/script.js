2e925c0309d16388f9d555890de37dc43fcfa46d
chiara1911 <chiaracesari89@gmail.com>
2023-10-11 16:38:28 +0200

    add square

--- js/script.js
+++ js/script.js
@@ -6,24 +6,26 @@ const wrapperEl = document.getElementById('boxWrapper');
 for (let i = 1; i < 101; i++) {
     //istruzioni da ripetere
     console.log(i);
-
+let bgBox;
     // se divisibile per 3 e 5
     if (i % 3 === 0 && i % 5 === 0) {
 
-        // bgBox = --color-bg-fizzbuzz
+        bgBox = 'fizzbuzz' ;
         // Text = fizzbuzz;
     } else if (i % 5 === 0) {
 
-        // bgBox = --color-bg-buzz:
+         bgBox = 'buzz';
         // Text = buzz;
     } else if (i % 3 === 0) {
 
-        // bgBox = --color-bg-fizz:
-        // text = fizz
+        bgBox = 'fizz';
+        // text = fizz;
 
     } else {
-        // bgBox = --color-bg-blue
-        //    text = i
+       bgBox = 'number';
+        //    text = i;
     }
-
+    const boxEl = document.createElement('div');
+    boxEl.className = `box ${bgBox}`;
+    wrapperEl.append(boxEl);
 }
