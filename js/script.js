df95537a59c1864f81b3a03171690d897cdbf0c5
chiara1911 <chiaracesari89@gmail.com>
2023-10-11 15:53:02 +0200

    add commit in js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,27 @@
 
 const wrapperEl = document.getElementById('boxWrapper');
 
-for(let i = 1; i <= 101; i++){
+for (let i = 1; i < 101; i++) {
     //istruzioni da ripetere
     console.log(i);
-   
+
+    // se divisibile per 3 e 5
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        // bgBox = --color-bg-fizzbuzz
+        // Text = fizzbuzz;
+    } else if (i % 5 === 0) {
+
+        // bgBox = --color-bg-buzz:
+        // Text = buzz;
+    } else if (i % 3 === 0) {
+
+        // bgBox = --color-bg-fizz:
+        // text = fizz
+
+    } else {
+        // bgBox = --color-bg-blue
+        //    text = i
+    }
+
 }
